675b1c0fbc429a6126a246ba4b169f2308b3bc67
BbillyMai <1363543881@qq.com>
2019-06-22 20:11:47 +0800

    add findById method

--- src/test/java/ItemRepositoryTestImpl.java
+++ src/test/java/ItemRepositoryTestImpl.java
@@ -5,4 +5,14 @@ public class ItemRepositoryTestImpl implements ItemRepository{
     public List<Item> findAll() {
         return TestData.ALL_ITEMS;
     }
+
+    @Override
+    public Item findById(String id) {
+        for(Item item: TestData.ALL_ITEMS){
+            if(id.equals(item.getId())){
+                return item;
+            }
+        }
+        return null;
+    }
 }
